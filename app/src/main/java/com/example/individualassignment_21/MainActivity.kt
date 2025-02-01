e47c1c5c417abbd986629978e7de218e3a2c0d84
NoahP-K <noahpk@bu.edu>
2025-02-01 12:23:45 -0500

    Added some comments.

--- app/src/main/java/com/example/individualassignment_21/MainActivity.kt
+++ app/src/main/java/com/example/individualassignment_21/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.individualassignment_21
 
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Icon
 import android.os.Bundle
-import android.text.style.ParagraphStyle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -18,25 +15,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -54,6 +45,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+//Makes the clock icon with text overlapping the bottom.
+//Used for the prep and cook time sections.
 @Composable
 fun drawClock(label: String) {
     Box(
@@ -78,6 +71,7 @@ fun drawClock(label: String) {
     }
 }
 
+//Makes a bulleted list of text from the given array of strings.
 @Composable
 fun bulletList(elements: Array<String>, modifier: Modifier) {
     val bullet = "\u2022"
@@ -95,6 +89,7 @@ fun bulletList(elements: Array<String>, modifier: Modifier) {
     )
 }
 
+//Make a card object containing a column of elements for the recipe card.
 @Composable
 fun MakeCard() {
     //A gray rectangular card with rounded corners
